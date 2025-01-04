ea59afed43673af41927257997467fc22905c7e1
Denis <dovzhanyndenis03@gmail.com>
2025-01-04 17:23:04 -0500

    feat: log-in page mostly complete/ needs finishing touches

--- src/login/Login.tsx
+++ src/login/Login.tsx
@@ -1,6 +1,5 @@
 import { motion } from "motion/react"
 import { useState, FormEvent } from "react";
-import LoginForm from "./loginForm/LoginForm";
 import './Login.css'
 import { easeInOut } from "motion"
 import { useDispatch, useSelector } from "react-redux";
@@ -10,6 +9,7 @@ import { logInAsync } from "../store/userSlice";
 function Login() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
     const {isLoading} = useSelector( (state: RootState) => state.user)
     const dispatch = useDispatch<AppDispatch>()
 
@@ -19,6 +19,10 @@ function Login() {
         // AND if they will be using a password OR one time use key to determine which endpoint to consume
         // here we probably need to somehow switch to a loading icon right?
             dispatch(logInAsync({username, password}))
+            // I guess if we are still here after this dispatch that means that it failed right?????
+            setUsername('');
+            setPassword('');
+            // actually we probably want to display an error on the input fields or somewhere on page if we get here
         // if valid we cache jwt here?
     }
 
@@ -38,11 +42,34 @@ function Login() {
         <div className="video-background">
             <video autoPlay loop muted playsInline>
             <source src="/loginBackground.mp4" type="video/mp4" />
-            Your browser does not support the video tag.
             </video>
         </div>
-        <LoginForm onSubmit={handleSubmit} onUsernameInputChange={handleUsernameInputChange}
-         onPasswordInputChange={handlePasswordInputChange}/>
+        
+        <motion.div id="loginForm">
+            <img src="/chattrlogo.png" className="logo"/>
+            <motion.img 
+            src="/chattrlogotext.png"
+            id="welcome"
+            initial={{opacity: 0}} 
+            animate={{opacity: [0.7, 1, 0.7]}}
+            transition={{repeat: Infinity, duration: 2, ease: easeInOut}} />
+                
+            <form onSubmit={handleSubmit} id="form">
+                <input className="input" type="text" name="username" placeholder="Username" onChange={(e) => handleUsernameInputChange(e.target.value)} required />
+                <input className="input" type={showPassword ? "text" : "password"} name="password" placeholder="Password" onChange={(e) => handlePasswordInputChange(e.target.value)} required />
+                <label className="showpassword">
+                    <input className="showpassword" type="checkbox" checked={showPassword} onChange={() => setShowPassword(!showPassword)} />
+                    Show password?
+                </label>
+                
+                <button type="submit" disabled={!(username.trim() && password.trim())}>Log in</button>
+            </form>
+            <a id="forgotpassword">Forgot password?</a>
+        </motion.div>
+        
+        <div id="registercontainer">
+            <label>No account? <a id="signup">Sign up</a></label>
+        </div>
        
     </div>
 }
