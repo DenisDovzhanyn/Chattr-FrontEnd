fba4389b69d652e846846bae8a14135e79ea1bb3
Denis <dovzhanyndenis03@gmail.com>
2025-01-31 14:06:07 -0500

    feat: calculates how long ago the last message received was for a chat and puts it next to chat name!

--- src/containers/ChatList.tsx
+++ src/containers/ChatList.tsx
@@ -4,11 +4,25 @@ import { loadChatsAsync } from '../store/chatSlice'
 import { useSelector, useDispatch } from 'react-redux'
 import { RootState, AppDispatch } from '../store/store'
 
+const calcTime = (timestamp: number) => {
+    const now = Date.now()
+    const past = new Date(timestamp)
 
+    const seconds = Math.floor((now - past.getTime()) / 1000)
+    if (seconds < 60) return `${seconds}s`
+    const minutes = Math.floor(seconds / 60)
+    if (minutes < 60) return `${minutes}m`
+    const hours = Math.floor(minutes / 60)
+    if (hours < 24) return `${hours}h`
+    const days = Math.floor(hours / 24)
+    if (days < 7) return `${days}d`
+    const weeks = Math.floor(days / 7)
+    return `${weeks}w`
+}
 function ChatList() {
     const {chats} = useSelector((state: RootState) => state.chats)
-    const {access} = useSelector((state: RootState) => state.user)
     const dispatch = useDispatch<AppDispatch>()
+
     useEffect(() => {
 
         dispatch(loadChatsAsync())
@@ -17,7 +31,12 @@ function ChatList() {
     return ( 
         <div id="chatlist">
             {chats.map((chat) => {
-                return <div>{chat.users.map((user) => user.display_name + " ")} </div>
+                return <div className='chat'>
+                    <div className='chatname'>
+                        {chat.users.map((user) => user.display_name + " ")}
+                    </div> 
+                    {calcTime(chat.last_msg_time)} 
+                </div>
             })}
         </div>
     )
