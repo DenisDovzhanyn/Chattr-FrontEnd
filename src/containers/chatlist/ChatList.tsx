8368d853c642a6d72d010b2d41fa86e5e29aeb0b
Denis <dovzhanyndenis03@gmail.com>
2025-02-08 14:19:41 -0500

    feat: create chat modal done

--- src/containers/chatlist/ChatList.tsx
+++ src/containers/chatlist/ChatList.tsx
@@ -44,11 +44,17 @@ function ChatList() {
 
             <div id='modalbackground' style={{display: createChatModalOpen ? 'block' : 'none'}} />
             <dialog open={createChatModalOpen} id='createchatmodal'>
+                <button onClick={() => (setCreateChatModalOpen(false))} id='modalexitbutton'>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" id='exitsvg'>
+                        <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/>
+                    </svg>
+                </button>
 
                 <label id='createchatlabel'>
                     Chat name
                     <input onChange={(e) => setNewChatName(e.target.value)} 
-                        className='input' 
+                        className='input'
+                        id='chatnameinput'
                         type='text' 
                         value={newChatName}
                         ref={inputRef}
@@ -56,19 +62,15 @@ function ChatList() {
                     />
                 </label>
 
-                <div id='modalbuttoncontainer'>
-                    <button onClick={() => (setCreateChatModalOpen(false))}>
-                        close
-                    </button>
-                    <button disabled = {newChatName ? false : true} onClick={(event) => {
-                        event.preventDefault()
-                        dispatch(createNewChatAsync(newChatName))
-                        setCreateChatModalOpen(false)
-                        setNewChatName('')
-                    }}>
-                        Create Chat
-                    </button>
-                </div>
+                <button id='modalbutton' disabled = {newChatName ? false : true} onClick={(event) => {
+                    event.preventDefault()
+                    dispatch(createNewChatAsync(newChatName))
+                    setCreateChatModalOpen(false)
+                    setNewChatName('')
+                }}>
+                    Create Chat
+                </button>
+
 
             </dialog>
             
