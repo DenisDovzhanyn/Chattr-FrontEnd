084a949a55121c2235fd69b62b5e3767446e799c
Denis <dovzhanyndenis03@gmail.com>
2025-02-19 16:54:03 -0500

    feat: added div that holds user input, automatically grows in size until stopping at 150px| added border under chat name so messages appear to go under it | messages are in their own container seperated from chat name / new message input

--- src/containers/chatLayout/chatLayout.tsx
+++ src/containers/chatLayout/chatLayout.tsx
@@ -1,11 +1,12 @@
 import './chatLayout.css'
 import { useSelector, useDispatch } from 'react-redux'
 import { RootState, AppDispatch } from '../../store/store'
-import { useMemo } from 'react'
+import { useMemo, useState } from 'react'
 import { calcTimeFull } from '../chatlist/ChatList'
 function ChatLayout() {
     const {currentlySelected} = useSelector((state: RootState) => state.chats)
-    
+    const [newMessage, setNewMessage] = useState('')
+
     const idToDisplay = useMemo(() => {
         const map = new Map<number, string>()
         currentlySelected?.users?.forEach(user => {
@@ -17,25 +18,37 @@ function ChatLayout() {
     if (currentlySelected === undefined) return <div> Please Select A Chat</div>
     if (currentlySelected.messages === undefined || currentlySelected.messages.length == 0) return <div>no messages for chat {currentlySelected.chat_name}</div>
     return (
-        <div id='chatlayout'>
-            <h2>chat: {currentlySelected.chat_name}</h2>
-            {currentlySelected.messages.map((message, index) => {
-                if (index != 0 && currentlySelected.messages![index - 1].user_id === message.user_id) {
-                    return <div className='messagebox'>
-                        {message.content}
+
+            <div id='chatlayout'>
+                <h2 id='chatname'>chat: {currentlySelected.chat_name}</h2>
+                <div id='messagecontainer'>
+                    {currentlySelected.messages.map((message, index) => {
+                        if (index != 0 && currentlySelected.messages![index - 1].user_id === message.user_id) {
+                            return <div className='messagebox'>
+                                {message.content}
+                                </div>
+                        }
+                        return <div className='messagebox' style={{marginTop: '20px'}}>
+                            <div className='displayname'>
+                                {idToDisplay.has(message.user_id) ? idToDisplay.get(message.user_id) : 'unknown user'}
+                                <div className='messagetime'>
+                                    {calcTimeFull(message.inserted_at) + ' ago'}
+                                </div>
+                            </div>
+                            {message.content}
                         </div>
-                }
-                return <div className='messagebox' style={{marginTop: '20px'}}>
-                    <div className='displayname'>
-                        {idToDisplay.has(message.user_id) ? idToDisplay.get(message.user_id) : 'unknown user'}
-                        <div className='messagetime'>
-                            {calcTimeFull(message.inserted_at) + ' ago'}
-                        </div>
-                    </div>
-                    {message.content}
+                    })}
                 </div>
-            })}
-        </div>
+
+                <div id='chatbox'>
+                    <div id='chatboxcontent'contentEditable="plaintext-only" onKeyDown={(e) => console.log(e)}>
+
+                    </div>
+                </div>
+            </div>
+            
+
+        
     )
 }
 
